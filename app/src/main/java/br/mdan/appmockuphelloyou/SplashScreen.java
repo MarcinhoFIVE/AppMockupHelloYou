a82f3198e264d41fc07e03b68bcba6208120be20
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-08-02 08:42:44 -0300

    Initial commit

---
+++ app/src/main/java/br/mdan/appmockuphelloyou/SplashScreen.java
@@ -0,0 +1,14 @@
+package br.mdan.appmockuphelloyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_layout);
+    }
+}
