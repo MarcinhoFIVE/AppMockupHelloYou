877e6731f11fa4c68415310be6d6bca6ed770f75
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-08-03 17:48:22 -0300

    Finished Layout And Start Activity Sequence

--- app/src/main/java/br/mdan/appmockuphelloyou/AccountScreen.java
+++ app/src/main/java/br/mdan/appmockuphelloyou/AccountScreen.java
@@ -3,12 +3,30 @@ package br.mdan.appmockuphelloyou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class AccountScreen extends AppCompatActivity {
+    EditText etName;
+    EditText etEmail;
+    EditText etPass;
+    ImageView btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_screen_layout);
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPass = findViewById(R.id.etPass);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Usuário Criado Com Sucesso !", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
