9cdb4b63bae986320fdc899d08ae672d06cefbc9
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-08-03 22:00:08 -0300

    Layout Remake

---
+++ app/src/main/java/br/mdan/appmockuphelloyou/ProfileScreen.java
@@ -0,0 +1,14 @@
+package br.mdan.appmockuphelloyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_screen_layout);
+    }
+}
